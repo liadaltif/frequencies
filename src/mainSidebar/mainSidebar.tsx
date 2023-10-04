216e2618d42096b504572964bbc0f81d3a2718f8
liadaltif <liadaltif@gmail.com>
2023-10-04 13:31:47 +0300

    Added buttons functionality to the new popup window, very cool.

--- src/mainSidebar/mainSidebar.tsx
+++ src/mainSidebar/mainSidebar.tsx
@@ -1,48 +1,78 @@
+// MainSidebar.tsx
+
 import React, { useState } from "react";
 import Draggable from "react-draggable";
 import "./MainSidebar.css";
 import OrgRelation from "../orgRelation/orgRelation";
 
-interface mainSidebarProps {
-  initialText?: string;
+interface MainSidebarProps {
+  initialHeaderText?: string;
+  initialButtonText?: string;
 }
 
-const MainSidebar: React.FC<mainSidebarProps> = ({
-  initialText = "שם גנרי למערכת",
+const MainSidebar: React.FC<MainSidebarProps> = ({
+  initialHeaderText = "שם גנרי למערכת",
+  initialButtonText = "hello",
 }) => {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [pressedButtons, setPressedButtons] = useState<string[]>([]);
 
   const togglePopup = () => {
     setIsPopupOpen(!isPopupOpen);
   };
 
-  const handleTextChange = (_event: React.KeyboardEvent<HTMLDivElement>) => {
-    // Handle text changes if needed
+  const handleClosePopup = () => {
+    setIsPopupOpen(false);
+  };
+
+  const handleButtonClick = (buttonName: string) => {
+    const newPressedButtons = pressedButtons.includes(buttonName)
+      ? pressedButtons.filter((btn) => btn !== buttonName)
+      : [...pressedButtons, buttonName];
+    setPressedButtons(newPressedButtons);
+  };
+
+  const getButtonText = () => {
+    return pressedButtons.length > 0
+      ? pressedButtons.join(",")
+      : initialButtonText;
   };
 
   return (
     <>
-      <Draggable>
+      <Draggable defaultPosition={{ x: 0, y: 0 }}>
         <div className="main-sidebar">
           <div
-            className="green-top"
+            className={`green-top ${
+              pressedButtons.length > 0 ? "pressed" : ""
+            }`}
             contentEditable={true}
-            onInput={handleTextChange}
           >
-            {initialText}
+            {initialHeaderText}
           </div>
           <div className="gray-bottom">
             <label>תחום התחלה</label>
             <input type="text" placeholder="תחום התחלה" />
             <label>תחום סוף</label>
             <input type="text" placeholder="תחום סוף" />
-            <button className="green-button" onClick={togglePopup}>
-              Open Popup
+            <button
+              className={`green-button ${
+                pressedButtons.length > 0 ? "pressed" : ""
+              }`}
+              onClick={togglePopup}
+            >
+              {getButtonText()}
             </button>
           </div>
         </div>
       </Draggable>
-      {isPopupOpen && <OrgRelation />}
+      {isPopupOpen && (
+        <OrgRelation
+          onClose={handleClosePopup}
+          onButtonClick={handleButtonClick}
+          pressedButtons={pressedButtons}
+        />
+      )}
     </>
   );
 };
--- src/orgRelation/orgRelation.tsx
+++ src/orgRelation/orgRelation.tsx
@@ -2,17 +2,41 @@ import React from "react";
 import Draggable from "react-draggable";
 import "./OrgRelation.css";
 
-interface OrgRelationProps {}
+interface OrgRelationProps {
+  onClose: () => void;
+  onButtonClick: (buttonName: string) => void;
+  pressedButtons: string[];
+}
 
-const OrgRelation: React.FC<OrgRelationProps> = () => {
+const OrgRelation: React.FC<OrgRelationProps> = ({
+  onClose,
+  onButtonClick,
+  pressedButtons,
+}) => {
+  const handleButtonClick = (buttonName: string) => {
+    onButtonClick(buttonName);
+  };
+
   return (
-    <Draggable>
+    <Draggable defaultPosition={{ x: 0, y: 0 }}>
       <div className="orgRelation-window">
         <div className="orgRelation-header">
           <span>Popup Window</span>
+          <button onClick={onClose}>Close</button>
         </div>
         <div className="orgRelation-content">
-          {/* Add content for the popup here */}
+          <button
+            onClick={() => handleButtonClick("Button 1")}
+            className={pressedButtons.includes("Button 1") ? "pressed" : ""}
+          >
+            Button 1
+          </button>
+          <button
+            onClick={() => handleButtonClick("Button 2")}
+            className={pressedButtons.includes("Button 2") ? "pressed" : ""}
+          >
+            Button 2
+          </button>
         </div>
       </div>
     </Draggable>
